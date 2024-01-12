a1dade1bbd41c704f1d44bc95fc246fe7d834442
temucode <magvan.temuulen@gmail.com>
2024-01-12 12:24:35 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class Api extends Component {
+  state = {
+    details: [],
+  };
+
+  componentDidMount() {
+    let data;
+    axios
+      .get('http://localhost:8000/api/monmodele/')
+      .then((res) => {
+        data = res.data;
+        this.setState({
+          details: data,
+        });
+      })
+      .catch((err) => {});
+  }
+
+  render() {
+    return (
+      <View style={StyleSheet.container}>
+        <Text style={StyleSheet.title}>Data Generated From Django</Text>
+        <View>
+          {this.state.details.map((output, id) => (
+            <View key={id}>
+              <Text>{output.nom}</Text>
+              <Text>{output.description}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: '#blue',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
   },
 });
+  
+
+export default Api;
